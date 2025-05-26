2fb9e17060c17f635bc713e82e36d0567095fa09
Ворощук Анна Николаевна <anvoroshchuk@edu.hse.ru>
2025-05-26 23:10:45 +0300

    Added pseudorandom number generator

---
+++ prng.py
@@ -0,0 +1,31 @@
+from hash import hash
+
+def prng(count: int):
+    """
+    Генератор псевдослучайных чисел.
+    count - нужное количество псевдослучайных чисел
+    Возвращает список из count элементов - битовых строк длины 256 бит.
+    """
+    # 512-битная строка из фамилии и имени
+    name = "Ворощук Анна"
+    bits = ''.join(f'{b:08b}' for b in name.encode('utf-8'))
+
+    if len(bits) < 512:
+        bits = bits.zfill(512)
+    else:
+        bits = bits[:512]
+
+    # Нулевой цикл - получаем h0
+    h0 = hash(bits)
+
+    random_numbers = []
+
+    # 3. Циклы i = 1..count
+    for i in range(1, count + 1):
+        # Формируем вход: h0 (256 бит) + i в 256 бит
+        i_bits = f'{i:0256b}'
+        input_bits = h0 + i_bits
+        h_i = hash(input_bits)
+        random_numbers.append(h_i)
+
+    return random_numbers
